1f11a6d09b645af0c03f7b4be3f72534944efe4a
Logan Snopek <LSnopek@lcs.on.ca>
2021-11-09 11:49:31 -0500

    set up my file and organized my files

---
+++ TradingCardsProgect/TradingCardLayout.swift
@@ -0,0 +1,87 @@
+//
+//  TradingCardLayout.swift
+//  TradingCardsProgect
+//
+//  Created by Logan Snopek on 2021-11-09.
+//
+
+import Foundation
+
+struct PlayerCard {
+    
+    let imageName: String
+    let name: String
+    let position: String
+    let hand: String
+    let hight: Double
+    let wiaght: Double
+
+    
+}
+
+let listOfPlayers = [
+    
+    PlayerCard(imageName:"Kevin Bahl",
+               name: "Kevin Bahl",
+               position: "Defence",
+               hand: "Left",
+               hight: 6.6,
+               wiaght: 230)
+    
+    ,
+
+    
+    PlayerCard(imageName: "Calen Addison",
+               name: "Calen Addison",
+               position: "Defence",
+               hand: "Right",
+               hight: 5.10,
+               wiaght: 180)
+    
+    ,
+
+    
+    PlayerCard(imageName: "Bowen Byram",
+               name: "Bowen Byram",
+               position: "Defence",
+               hand: "Left",
+               hight: 6.1,
+               wiaght: 197)
+    
+    ,
+    
+    PlayerCard(imageName: "Jacob Bernard-Docker",
+               name: "Jacob Bernard-Docker",
+               position: "Defence",
+               hand: "Right",
+               hight: 6.0,
+               wiaght: 190)
+    
+    ,
+    
+    PlayerCard(imageName: "Jamie Drysdale",
+               name: "Jamie Drysdale",
+               position: "Defence",
+               hand: "Right",
+               hight: 5.11,
+               wiaght: 180)
+    
+    ,
+    
+    PlayerCard(imageName: "Jared McIsaac",
+               name: "Jared McIsaac",
+               position: "Defence",
+               hand: "Left",
+               hight:6.1,
+               wiaght: 191)
+    
+    ,
+    
+    PlayerCard(imageName: "Ty Smith",
+               name: "Ty Smith",
+               position: "Defence",
+               hand: "Left",
+               hight: 5.11,
+               wiaght: 175)
+    
+]
---
+++ TradingCardsProgect/Views/TradingCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TradingCardsProgect
+//
+//  Created by Logan Snopek on 2021-11-09.
+//
+
+import SwiftUI
+
+struct PlayerCardView: View {
+
+}
+
+struct PlayerCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerCardView()
+    }
+}
