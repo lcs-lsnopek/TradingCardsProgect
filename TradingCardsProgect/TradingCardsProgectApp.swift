7e76e1ffc5d7e639588057478beb8e59b124c820
Logan Snopek <LSnopek@lcs.on.ca>
2021-11-09 11:06:43 -0500

    Initial Commit

---
+++ TradingCardsProgect/TradingCardsProgectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsProgectApp.swift
+//  TradingCardsProgect
+//
+//  Created by Logan Snopek on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsProgectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
